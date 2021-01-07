3ec2da447f7050605bee91da7e9bc547177d3fcd
mueenou <mueen.hossain@gmail.com>
2021-01-07 03:25:38 +0100

    added: jwt verify

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,6 @@ const router = require('express').Router()
 const User = require('../model/user')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-
-dotenv.config()
 
 const {registerValidation, loginValidation} = require('../validation')
 
